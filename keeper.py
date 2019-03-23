72a303cf39308b55541ccf625cf028af0b7f0a48
Pedro Vieira-PC <pedrovieira3333@hotmail.com>
2019-03-23 14:37:21 +0000

    Update v0.2

--- keeper.py
+++ keeper.py
@@ -1,8 +1,9 @@
-import win32con
 import win32api
 import win32gui
-import sys
-import time
+from threading import Timer
+
+import win32con
+
 #
 # def log_info(msg):
 #     """ Prints """
@@ -55,51 +56,80 @@ import time
 #         win32gui.PumpWaitingMessages()
 #         time.sleep( 1 )
 
+
+#Get the current ThreadID to later quit the program
+main_thread_id = win32api.GetCurrentThreadId()
+
+# Function to stop the program when called
+
+def on_timer():
+    win32api.PostThreadMessage(main_thread_id, win32con.WM_QUIT, 0, 0);
+
+t = Timer(5.0, on_timer) # Quit after 5 seconds
+
+
+# ------------------------------------ #
 def log_info(msg):
-     """ Prints """
-     print (msg)
-     f = open("C:\\Users\User\Key.txt", "a")
-     f.write(msg + "\n")
-     f.close()
+    """ Prints """
+    print( msg )
+    f = open( "C:\\Users\\User\\Log.txt", "a" )
+    f.write( msg + "\n" )
+    f.close()
+# ----------------------------------- #
 
+
+# Reads the messages from the message map and prints
 def wndproc(hwnd, msg, wparam, lparam):
-    log_info("wndproc: %s" % msg)
+    ctrl = msg
+    log_info( '%s' % msg )
 
+
+# ---------------------------------- #
+
+#   Test class
+#   Used to create and test how to grab windows messages
+#   Test "virtually posted" and "real queue" messages
+#   https://devblogs.microsoft.com/oldnewthing/?p=33453
+
+#----------------------------------- #
 
 class MyWindow:
-    log_info("***STARTING***")
+    log_info( "***STARTING***" )
+
     def __init__(self):
         win32gui.InitCommonControls()
-        self.hinst = win32api.GetModuleHandle(None)
+        self.hinst = win32api.GetModuleHandle( None )
         className = 'MyWndClass'
-        message_map = { win32con.WM_QUERYENDSESSION : wndproc,
-                        win32con.WM_ENDSESSION : wndproc,
-                        win32con.WM_QUIT : wndproc,
-                        win32con.WM_DESTROY : wndproc,
-                        win32con.WM_CLOSE : wndproc,
-                        win32con.CTRL_C_EVENT : wndproc,
+        message_map = {
+            win32con.WM_QUERYENDSESSION: wndproc,
+            win32con.WM_ENDSESSION: wndproc,
+            win32con.WM_QUIT: wndproc,
+            win32con.WM_DESTROY: wndproc,
+            win32con.WM_CLOSE: wndproc,
+            win32con.CTRL_C_EVENT: wndproc,
         }
+
         wc = win32gui.WNDCLASS()
         wc.style = win32con.CS_HREDRAW | win32con.CS_VREDRAW
         wc.lpfnWndProc = message_map
         wc.lpszClassName = className
-        win32gui.RegisterClass(wc)
+        win32gui.RegisterClass( wc )
         style = win32con.WS_OVERLAPPEDWINDOW
         self.hwnd = win32gui.CreateWindow(
-            className,
-            'My win32api app',
-            style,
-            win32con.CW_USEDEFAULT,
-            win32con.CW_USEDEFAULT,
-            300,
-            300,
-            300,
-            0,
-            self.hinst,
-            None
+                className,
+                'My win32api app',
+                style,
+                win32con.CW_USEDEFAULT,
+                win32con.CW_USEDEFAULT,
+                300,
+                300,
+                300,
+                0,
+                self.hinst,
+                None
         )
-        win32gui.UpdateWindow(self.hwnd)
-        win32gui.ShowWindow(self.hwnd, win32con.SW_SHOW)
+        win32gui.UpdateWindow( self.hwnd )
+        win32gui.ShowWindow( self.hwnd, win32con.SW_SHOW )
 
 
 w = MyWindow()
