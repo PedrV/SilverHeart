6d0752e464f96fc1b91a0a04eb2997c17da948a3
Pedro Vieira <pedrovieira3333@hotmail.com>
2019-04-02 21:46:06 +0100

    Update executerOS.py

--- macOS _ Linux/executerOS.py
+++ macOS _ Linux/executerOS.py
@@ -5,7 +5,7 @@ import logger
 
 if __name__ == '__main__':
     p1 = mp.Process( target=keeperOS.keeperOS )
-    p2 = mp.Process( target=logger.logger )
+    p2 = mp.Process( target=loggerOS.loggerOS )
     p1.start()
     p2.start()
     p1.join()
