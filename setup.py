3c5dd8930b61b1896293f45989181c579f5c265b
Pedro Vieira <pedrovieira3333@hotmail.com>
2019-04-02 18:54:57 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name= 'SilverHeart',
+    version = 1.0,
+    author= 'Pedro Vieira',
+    author_email= 'pedrovieira3333@protonmail.com',
+    license='GPLv3+',
+    description='Keylogger developed in python',
+    long_description= long_description,
+    url='https://github.com/PedrV/SilverHeart',
+    packages= ['Windows', 'OS X_Linux'],
+    python_requires='>=3.7',
+    install_requires= [
+        'pynput>=1.4.2',
+        'Pywin32>=224',
+        ],
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Licence :: OSI Approved :: GPLv3+ license',
+        'Operating System :: Windows',
+    ]
+
+)
